b9ff93801ef4fc4d11c9294a90b795823bd2013f
xelfus23 <xelfus23@gmail.com>
2025-03-21 17:18:50 +0800

    update

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: ["https://deploy-mern/patrick-web.vercel.app"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
 
 const PORT = process.env.PORT || 5000; // Provide a default port
 const MONGODB = process.env.MONGODB_URL;
